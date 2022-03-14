72b8d2819771ba49ac61f19fd4d852c618a5139e
AslamJM <88993157+AslamJM@users.noreply.github.com>
2022-03-14 14:59:09 +0530

    some last changes

--- src/components/footer/Social.jsx
+++ src/components/footer/Social.jsx
@@ -8,10 +8,10 @@ const Social = () => {
         <div className={styles.socialIcons}>
             <p>Follow Us</p>
             <div className={styles.socialIconsContainer}>
-              <div className={styles.iconbox}><a href="https://facebook.com" target="_blank" rel="noopener norefferer"><GrFacebook className={styles.icons}/></a></div>
+              <div className={styles.iconbox}><a href="https://www.facebook.com/emdlabs/" target="_blank" rel="noopener norefferer"><GrFacebook className={styles.icons}/></a></div>
               <div className={styles.iconbox}><a href="https://twitter.com" target="_blank" rel="noopener norefferer"><GrTwitter className={styles.icons}/></a></div>
               <div className={styles.iconbox}><a href="https://instagram.com" target="_blank" rel="noopener norefferer"><GrInstagram className={styles.icons}/></a></div>
-              <div className={styles.iconbox}><a href="https://linkedin.com" target="_blank" rel="noopener norefferer"><GrLinkedin className={styles.icons}/></a></div>
+              <div className={styles.iconbox}><a href="https://www.linkedin.com/company/emdlabs/?originalSubdomain=lk" target="_blank" rel="noopener norefferer"><GrLinkedin className={styles.icons}/></a></div>
             </div>
         </div>
     </div>
