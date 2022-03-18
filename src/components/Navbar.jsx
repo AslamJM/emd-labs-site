d30d8cbee3eb25d1ff8c422adaffe1792f4a8169
AslamJM <88993157+AslamJM@users.noreply.github.com>
2022-03-18 12:08:24 +0530

    nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const activeStyle={
 
 const Navbar = () => {
 
-    const[showMenu,setShowMenu]=useState(true)
+    const[showMenu,setShowMenu]=useState(false)
 
     const logoImg=useStaticQuery(graphql`{
 file(relativePath: {eq: "images/logo1.png"}) {
