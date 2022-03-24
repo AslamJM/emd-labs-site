9ec6f8a5dffdaf6110c014e8b26400a4b02f0321
aslam jm <aslamjm@aslams-MacBook-Pro.local>
2022-03-24 17:38:56 +0530

    nearing the end

--- src/components/hero/NewHero.jsx
+++ src/components/hero/NewHero.jsx
@@ -3,13 +3,8 @@ import React from 'react'
 import * as styles from '../../styles/landing/newHero.module.scss'
 import { Carousel } from 'react-responsive-carousel';
 import SliderItem from './Slider';
-import { useStaticQuery,graphql } from 'gatsby';
-import ParticleJS from '../ParticleJS'
-//import "slick-carousel/slick/slick.css";
-//import "slick-carousel/slick/slick-theme.css";
-//import Slider from "react-slick";
-
-
+import { useStaticQuery, graphql } from 'gatsby';
+//import ParticleJS from '../ParticleJS'
 
 const NewHero = () => {
 
@@ -26,24 +21,23 @@ const NewHero = () => {
   }
 }`)
 
-const images = imgQuery.allFile.nodes
-   
-const items = [<SliderItem title="Industrial revolution 4.0" text="We process automation and data exchange using advanced manufacturing technologies in industries" img={images[0]}/>,<SliderItem text="We process automation and data exchange using advanced manufacturing technologies in industries" title="DevOps & Agile" img={images[1]}/>,<SliderItem text="We understand customers’ needs, rapid prototyping, and generating creative ideas which will transform the way of developing products, services, and processes." title="Design thinking" img={images[2]}/>,<SliderItem text="We provide solutions that meet the needs of present problems with the future forecast of socio, economic and environmental benefits" title="Sustainable Solutions" img={images[3]}/>]
+  const images = imgQuery.allFile.nodes
+
+  const items = [<SliderItem title="Industrial revolution 4.0" text="We process automation and data exchange using advanced manufacturing technologies in industries" img={images[0]} />, <SliderItem text="We process automation and data exchange using advanced manufacturing technologies in industries" title="DevOps & Agile" img={images[1]} />, <SliderItem text="We understand customers’ needs, rapid prototyping, and generating creative ideas which will transform the way of developing products, services, and processes." title="Design thinking" img={images[2]} />, <SliderItem text="We provide solutions that meet the needs of present problems with the future forecast of socio, economic and environmental benefits" title="Sustainable Solutions" img={images[3]} />]
 
 
 
   return (
     <div className={styles.newhero}>
-      <ParticleJS/>
-        <div className={styles.container} >
-            <div>
-              <Carousel autoPlay infiniteLoop showArrows={false} showStatus={false} showThumbs={false} width="100%" >
-                {
-                  items.map(item=>item)
-                }
-              </Carousel>
-            </div>
+      <div className={styles.container} >
+        <div>
+          <Carousel autoPlay infiniteLoop showArrows={false} showStatus={false} showThumbs={false} width="100%" >
+            {
+              items.map(item => item)
+            }
+          </Carousel>
         </div>
+      </div>
     </div>
   )
 }
