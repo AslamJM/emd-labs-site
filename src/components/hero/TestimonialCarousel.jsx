63af822fc0b83bdec1a4ed1ebef7859ba2b3d3c2
aslam jm <aslamjm@aslams-MacBook-Pro.local>
2022-04-08 16:05:36 +0530

    testimonials added

--- src/components/hero/TestimonialCarousel.jsx
+++ src/components/hero/TestimonialCarousel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Testimonials from './Testimonials'
 import {useStaticQuery,graphql} from 'gatsby'
 import * as styles from '../../styles/landing/testimonials.module.scss'
-
+import { Carousel } from 'react-responsive-carousel'    
 const TestimonialCarousel = () => {
 
   const data = useStaticQuery(graphql`query MyQuery {
@@ -27,12 +27,14 @@ const nodes=data.allMarkdownRemark.nodes
     <div className={styles.carousel}>
         <h3 className={styles.header}>testimonials</h3>
         <div className={styles.container}>
+          <Carousel>
             {
               nodes.map((node)=>{
                 const {name,position,featuredImage}=node.frontmatter
                 return<Testimonials name={name} position={position} image={featuredImage} html={node.html} key={name}/>
               })
             }
+            </Carousel>
         </div>
     </div>
   )
